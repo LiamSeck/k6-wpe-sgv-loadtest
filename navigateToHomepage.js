9decd299706310a4e5ff2d4e24c35f58887fdbb0
Liam Seck <liamseck@live.ie>
2025-04-25 17:10:52 +0100

    updating comment

--- navigateToHomepage.js
+++ navigateToHomepage.js
@@ -5,8 +5,8 @@ import { checkStatus } from "./utils.js";
 import { randomIntBetween } from "https://jslib.k6.io/k6-utils/1.1.0/index.js";
 
 export function navigateToHomepage() {
-  group("Navigate to HomePage: https://shegrowsveg.com/", function () {
-    // Make GET request to Home Page https://shegrowsveg.com/
+  group("Navigate to HomePage: https://shegrowsvegstg.wpenginepowered.com/", function () {
+    // Make GET request to Home Page https://shegrowsvegstg.wpenginepowered.com/
     let response = http.get(`https://${base_url}/`, {
         headers: {
             'upgrade-insecure-requests': '1',
